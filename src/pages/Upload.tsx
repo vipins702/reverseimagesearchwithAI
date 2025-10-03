689b9bd6fbe7322319370be4961f2ff6e00904fe
vipins702 <vipins702@gmail.com>
2025-10-03 11:42:06 +0530

    FIX SCHEMA: Add name property to BreadcrumbList to eliminate 'Unnamed item' in Google Rich Results Test

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -323,6 +323,7 @@ const UploadPage = () => {
         {
           "@context": "https://schema.org",
           "@type": "BreadcrumbList",
+          "name": "Site Navigation",
           "itemListElement": [
             {
               "@type": "ListItem",
